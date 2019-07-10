72d8d0f877a24b8ac903f90b0b09f166a01ce428
chic-page-6502 <zacharytq@gmail.com>
2019-07-10 01:49:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "Fizz"
+    "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0
-    "FizzBuzz"
+    "Fizz"
   end
 end
